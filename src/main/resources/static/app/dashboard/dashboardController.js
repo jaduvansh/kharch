5c2d65daf9956ab9f987a1febbd3087307b9d501
jaduvansh <jaduvansh@gmail.com>
2018-04-15 08:04:11 +0530

    Added total functionality

--- src/main/resources/static/app/dashboard/dashboardController.js
+++ src/main/resources/static/app/dashboard/dashboardController.js
@@ -10,11 +10,20 @@ angular.module('kharchApp').controller('DashboardController',
     		if($scope.user){
     			dashboardService.gridData($scope.user.userName).then(function(grid){
 		    		 $scope.grid = grid;
+		    		 $scope.spentAmount = calculateSpentAmount(grid.footer);
 		    	}, function(error){
 		    		alert("error loading data");
 		    	});
     		}
 	    	
+    		var calculateSpentAmount = function(footer){
+    			var spentAmount = 0;
+    			for(var i=0; i<footer.length ; i++){
+    				spentAmount += footer[i].amount;
+    			}
+    			console.log("spentAmount " +spentAmount);
+    			return spentAmount;
+    		};
 	    	
 	    	$scope.logout = function(){
 	    		userService.clearUser();
--- src/main/resources/static/app/dashboard/dashboardService.js
+++ src/main/resources/static/app/dashboard/dashboardService.js
@@ -1,52 +1,73 @@
 (function(){
-'use strict';
-angular.module('kharchApp').factory('DashboardService',['ExpenditureService', '$q', 'LookupTypeService', dashboardService]);
-
-    function dashboardService(expenditureService, $q, lookupTypeService) {
-    	
-    	var arrangeRowBasedOnHeaderOrder = function(header, row){
-    		var result = {};
-	    	result.date = row.date;
-			result.expTypes = [];
-			for(var i=0; i< header.length; i++){
-				var expTypeObj = row.expType[header[i]];
-				if(expTypeObj){
-					result.expTypes.push(expTypeObj);
-				} else {
-					result.expTypes.push({});
+	'use strict';
+	angular.module('kharchApp').factory('DashboardService',['ExpenditureService', '$q', 'LookupTypeService', dashboardService]);
+	
+		function dashboardService(expenditureService, $q, lookupTypeService) {
+			
+			var arrangeRowBasedOnHeaderOrder = function(header, row){
+				var result = {};
+				result.date = row.date;
+				result.expTypes = [];
+				for(var i=0; i< header.length; i++){
+					var expTypeObj = row.expType[header[i]];
+					if(expTypeObj){
+						result.expTypes.push(expTypeObj);
+					} else {
+						result.expTypes.push({});
+					}
+					
 				}
-				
+				return result;
+			};
+			
+			var arrangeBodyBasedOnHeaderOrder = function(header, searchResult){
+				var rows = [];
+				for(var i=0; i< searchResult.length; i++){
+					rows.push(arrangeRowBasedOnHeaderOrder(header, searchResult[i]));
+				}
+				return rows;
+			};
+			
+			var populateFooterBasedOnHeader = function(header, searchResult){
+				var footer = [];
+				for(var j=0; j< header.length; j++){
+					var footerObj = {};
+					footerObj.type = header[j];
+					footerObj.amount = calculateFooterTotal(header[j], searchResult);
+					footer.push(footerObj);
+				}
+				return footer;
+			};
+			
+			var calculateFooterTotal = function(headerType,searchResult){
+				var amount =  0;
+				for(var i=0; i< searchResult.length; i++){
+					amount += searchResult[i].expType[headerType] ? searchResult[i].expType[headerType].amount : 0;
+				}
+				return amount;
 			}
-			return result;
-    	};
-    	
-    	var arrangeBodyBasedOnHeaderOrder = function(header, searchResult){
-    		var rows = [];
-    		for(var i=0; i< searchResult.length; i++){
-    			rows.push(arrangeRowBasedOnHeaderOrder(header, searchResult[i]));
-    		}
-    		return rows;
-    	};
-    	
-    	var gridData = function(username){
-    		var grid={};
-    		var deffered = $q.defer();
-    		$q.all([lookupTypeService.getExpenditureTypes(),
-    				expenditureService.searchAllExpenditureByUserName(username)]).then(function(response){
-    					grid.header = response[0].data;
-    					console.log(grid.header);
-    				    grid.body = arrangeBodyBasedOnHeaderOrder(grid.header, response[1]);
-    				    console.log(grid.body);
-    				    deffered.resolve(grid);
-    			  },function(error){
-    				  deffered.reject(error);
-    			  });
-    		return deffered.promise;
-    	}
-    	
-    	return {
-    		gridData : gridData
-    		
-    	};
-    }
- })();   
+
+			var gridData = function(username){
+				var grid={};
+				var deffered = $q.defer();
+				$q.all([lookupTypeService.getExpenditureTypes(),
+						expenditureService.searchAllExpenditureByUserName(username)]).then(function(response){
+							grid.header = response[0].data;
+							console.log(grid.header);
+							grid.body = arrangeBodyBasedOnHeaderOrder(grid.header, response[1]);
+							console.log(grid.body);
+							grid.footer = populateFooterBasedOnHeader(grid.header, response[1]);
+							console.log(grid.footer);
+							deffered.resolve(grid);
+					  },function(error){
+						  deffered.reject(error);
+					  });
+				return deffered.promise;
+			}
+			
+			return {
+				gridData : gridData
+				
+			};
+		}
+	 })();   
